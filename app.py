9145fab74c17c2075abbf1b612afad85f46ab062
wsgong94 <68831970+wsgong94@users.noreply.github.com>
2020-07-27 22:36:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,21 @@ def add_numbers_post():
   	      except ValueError:
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 
+@app.route('/add', methods=['GET','POST'])
+def add_numbers_post():
+	  # --> ['5', '6', '8']
+	  # print(type(request.form['text']))
+	  if request.method == 'GET':
+	  	return render_template('add_numbers.html')
+	  elif request.method == 'POST':
+  	      print(request.form['text'].split())
+  	      total = 0
+  	      try:
+  	      	for str_num in request.form['text'].split():
+  	      		total += int(str_num)
+  	      	return render_template('add_numbers.html', result=str(total))
+  	      except ValueError:
+  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"	
 
 @app.route('/shopping_list', methods=['GET','POST'])
 def shopping_list_post():
